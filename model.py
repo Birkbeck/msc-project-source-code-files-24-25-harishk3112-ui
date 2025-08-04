35ff2eb00960d0a168dc4ed0ce0a1def9c8b48e0
Harish Krishnakumar <harishk3112@gmail.com>
2025-08-04 17:40:22 +0100

    Load and parse household energy dataset

--- model.py
+++ model.py
@@ -3,3 +3,11 @@ import numpy as np
 from statsmodels.tsa.arima.model import ARIMA
 import warnings
 warnings.filterwarnings("ignore")
+
+def load_and_train_model():
+    df = pd.read_csv("data/household_power_consumption.txt",
+                     sep=';',
+                     parse_dates=[[0, 1]],
+                     infer_datetime_format=True,
+                     low_memory=False,
+                     na_values=['?'])
