b3de7e9c887fbe854a7b9646b325108f31ce67f2
Harish Krishnakumar <harishk3112@gmail.com>
2025-08-11 17:16:48 +0100

    Imported libraries for data preprocessing, LSTM model building, and file handling

--- model.py
+++ model.py
@@ -4,3 +4,13 @@ import os
 # Reduce TensorFlow logs and disable some CPU optimizations
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
+
+# Import required libraries 
+import numpy as np
+import pandas as pd
+from collections import deque
+from sklearn.preprocessing import StandardScaler
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import LSTM, Dense, Dropout
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from pathlib import Path
