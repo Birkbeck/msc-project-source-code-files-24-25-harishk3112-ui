6133b57ef89c0dc752a97c04cd477c6c8cc4f852
Harish Krishnakumar <harishk3112@gmail.com>
2025-08-11 20:03:50 +0100

    route to handle specific path requests

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__, static_folder="static", template_folder="templates")
 def healthz():
     return "ok", 200
 
+@app.route('/.well-known/<path:subpath>')
+def well_known(subpath):
+    return ('', 204)
+
 @app.route('/')
 def home():
     return render_template("index.html")
